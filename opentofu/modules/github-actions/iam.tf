9be38f911885910d118dfbc5f305ddec917d0915
micxer <micxer@micxer.de>
2026-09-15 18:24:25 +0200

    Fix Trivy parsing error in IAM policy

--- opentofu/modules/github-actions/iam.tf
+++ opentofu/modules/github-actions/iam.tf
@@ -191,7 +191,7 @@ resource "aws_iam_policy" "infrastructure_opentofu_api" {
           "iam:GetPolicy",
           "iam:DeletePolicy",
           "iam:GetPolicyVersion",
-          "iam:ListPolicyVersions"
+          "iam:ListPolicyVersions",
           "iam:CreatePolicyVersion",
           "iam:TagRole",
           "iam:UntagRole",
